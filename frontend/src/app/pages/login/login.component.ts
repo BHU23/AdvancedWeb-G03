fde4423a551927f19d4b3fe3e07429bef094fa84
Bhuwadol <125109678+BHU23@users.noreply.github.com>
2024-09-16 17:43:15 +0700

    add pages login sign up - close #14, - close #15

--- frontend/src/app/pages/login/login.component.ts
+++ frontend/src/app/pages/login/login.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth/auth.service'; // Adjust the path as needed
+import { Router } from '@angular/router';
+interface LoginResponse {
+  token: string;
+  result: any; 
+  status: boolean;
+}
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {
+  
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup; // Define loginForm as a FormGroup
+  submitted = false;
+  errorMessage: string | null = null;
 
+  constructor(
+    private fb: FormBuilder, // Inject FormBuilder
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    // Initialize the form in ngOnInit using FormBuilder
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
+
+  get f() {
+    return this.loginForm.controls;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    this.authService.login(this.loginForm.value).subscribe(
+      (response: LoginResponse) => {
+        const storage = this.loginForm.value.rememberMe
+          ? localStorage
+          : sessionStorage;
+        storage.setItem('token', response.token);
+        this.router.navigate(['/']); 
+      },
+      (error: any) => {
+        this.errorMessage = error.error?.message || 'Login failed';
+      }
+    );
+  }
 }

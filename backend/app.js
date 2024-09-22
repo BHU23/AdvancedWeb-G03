5a6a3f804f4e068c6bdb6ec7bcf0bee2dffee27d
Bhuwadol <125109678+BHU23@users.noreply.github.com>
2024-09-22 22:57:46 +0700

    update CORS and setname databass in env file

--- backend/app.js
+++ backend/app.js
@@ -9,16 +9,24 @@ const authRoutes = require('./routes/auth');
 const authMiddleware = require('./middleware/auth');
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200'); // อนุญาตให้เข้าถึงจากโดเมนนี้
     res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PUT, PATCH, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Option, Authorization');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    
+    // ถ้าเป็น OPTIONS request ให้ส่งกลับ 204 No Content
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(204);
+    }
+
     next();
 });
+
 app.use(cors({
   origin: 'http://localhost:4200',
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
+
 app.use(express.json());
 
 app.use('/auth', authRoutes);
--- backend/config/index.js
+++ backend/config/index.js
@@ -1,8 +1,12 @@
+require('dotenv').config();
+
 module.exports = {
   port: process.env.PORT || 3000,
   db: {
-    host: 'localhost',
-    port: 27017,
-    name: 'AW-01'
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    name: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD
   }
 };

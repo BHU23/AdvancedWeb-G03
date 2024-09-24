10b5eb291a7a201e76e7ce4be859d6439838293d
Bhuwadol <125109678+BHU23@users.noreply.github.com>
2024-09-24 18:13:22 +0700

    update header for can logout and down version boostrap

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth/auth.service';
-
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -9,9 +10,29 @@ import { AuthService } from '../../services/auth/auth.service';
 export class HeaderComponent implements OnInit {
   isLoggedIn: boolean = false;
 
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private location: Location
+  ) {}
 
   ngOnInit(): void {
     this.isLoggedIn = this.authService.isLoggedIn();
+  }
+
+  onLogout(): void {
+    this.authService.logout();
+    localStorage.removeItem('token');
+    sessionStorage.removeItem('token');
+    this.router.navigate(['/login']).then(() => {
+      window.location.reload();
+    });
+  }
+
+  private reloadCurrentRoute(): void {
+    const currentUrl = this.router.url;
+    this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
+      this.router.navigate([currentUrl]);
+    });
   }
 }

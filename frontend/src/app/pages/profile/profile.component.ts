3cfd484e4628175e99ec5c6e7c0b6768c7b042d4
Bhuwadol <125109678+BHU23@users.noreply.github.com>
2024-09-24 20:34:48 +0700

    update user data in profile

--- frontend/src/app/pages/profile/profile.component.ts
+++ frontend/src/app/pages/profile/profile.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Service } from '../../services/shareServices/plannig-noti.service';
+import { DecodedToken } from '../../services/auth/auth.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.css'
+  styleUrls: ['./profile.component.css'], // Fix typo: 'styleUrl' should be 'styleUrls'
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit, OnDestroy {
+  userData: DecodedToken | null = null; // To hold the user data
+  private userDataSubscription!: Subscription; // To manage subscription
 
+  constructor(private userService: Service) {}
+
+  ngOnInit(): void {
+    // Subscribe to userData$ observable
+    this.userDataSubscription = this.userService.userData$.subscribe((data) => {
+      this.userData = data;
+      console.log('User Data in ProfileComponent:', this.userData);
+    });
+  }
+
+  ngOnDestroy(): void {
+    // Unsubscribe to avoid memory leaks
+    if (this.userDataSubscription) {
+      this.userDataSubscription.unsubscribe();
+    }
+  }
 }
